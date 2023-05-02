eb0e054fcdbeae73c11e548cc10d213d33ea7be0
Ravi <raviredford@yahoo.com>
2023-05-02 17:05:29 +0530

    final changes

--- interface.tf
+++ interface.tf
@@ -41,6 +41,6 @@ output "adresses" {
     value = "${aws_instance.web.*.public_ip}"
 }
 
-output "public_subnet_id" {
+/* output "public_subnet_id" {
     value = "${module.vpc.public_subnet_id}"
-}
+} */
